f7d23f51ec37c9d2b3b323a170ee96425292d35c
seba2803 <sebaovelazquez@gmail.com>
2023-05-03 23:01:22 -0300

    new

--- archivo-ejemplo.js
+++ archivo-ejemplo.js
@@ -91,3 +91,9 @@ console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
   }
   console.log(spinalCase('thisIsSpinalTap'));
 
+
+  function deleteElements(arr){
+  let newlista = new Set(arr);//el objeto Set no acepta elementos repetidos
+  return [...newlista];
+}
+console.log(deleteElements([1,1,2,2,3,4,5,5]));
---
+++ practica.js
@@ -0,0 +1,7 @@
+function deleteElements(arr){
+    let newlista = new Set(arr);//el objeto Set no acepta elementos repetidos
+    return [...newlista];
+  }
+  console.log(deleteElements([1,1,2,2,3,4,5,5]));
+
+  let obj = {a: 'hola', b: 'Alla'};
